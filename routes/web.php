02f40b755af4f9eeb13079f72664205ad77e4abd
Berlkot <romicheg0600@mail.ru>
2024-11-23 02:19:57 +0300

    Add comment section

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CommentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,19 +22,24 @@ use App\Http\Controllers\ArticleController;
 Route::get('/auth/signup', action: [AuthController::class, 'signup']);
 Route::post('/auth/register', action: [AuthController::class, 'register']);
 Route::get('/', action: [MainController::class, 'index']);
-Route::get('/gallery/{img}/{name}', function($img, $name) {
-    return view('main.gallery', ['img'=>$img, 'name'=>$name]);
+Route::get('/gallery/{img}/{name}', function ($img, $name) {
+    return view('main.gallery', ['img' => $img, 'name' => $name]);
 });
-Route::get('/about', function() {
+Route::get('/about', function () {
     return view('main.about');
 });
-Route::get('/contacts', function() {
+Route::get('/contacts', function () {
     $data = [
-        'city'=>'Moscow',
-        'street'=>'Semenovskaya',
-        'house'=>38,
+        'city' => 'Moscow',
+        'street' => 'Semenovskaya',
+        'house' => 38,
     ];
-    return view('main.contact', ['data'=> $data]);
+    return view('main.contact', ['data' => $data]);
 });
 
 Route::resource('articles', ArticleController::class);
+
+Route::post('/comment', [CommentController::class, 'store']);
+Route::get('/comment/{id}/edit', [CommentController::class, 'edit']);
+Route::post('/comment/{comment}/update', [CommentController::class, 'update']);
+Route::get('/comment/{id}/delete', [CommentController::class, 'delete']);
